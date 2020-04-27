8683640e5b2eb10fb6a5438be2a3603b5b515cdb
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:18:41 -0500

    Update DetectPoisonAndDisease.java

--- 1st Level/DetectPoisonAndDisease.java
+++ 1st Level/DetectPoisonAndDisease.java
@@ -2,6 +2,8 @@ public class DetectPoisonAndDisease extends Spell {
   
   private int Level = 1;
   
+  private String name = "Detect Poison and Disease";
+  
   private String ClassRequirement[] = {"Cleric"};
   
   private String Description = "Spell: Detect Poison and Disease\n"+
@@ -29,6 +31,11 @@ public class DetectPoisonAndDisease extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
