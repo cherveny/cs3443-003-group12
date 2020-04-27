189fd2e10097dd2ac84b8de65f57fbeabaec1bc9
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:30:49 -0500

    Update IceKnife.java

--- 1st Level/IceKnife.java
+++ 1st Level/IceKnife.java
@@ -2,6 +2,8 @@ public class IceKnife extends Spell {
   
   private int Level = 1;
   
+  private String name = "Ice Knife";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Ice Knife\n"+
@@ -29,6 +31,11 @@ public class IceKnife extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
