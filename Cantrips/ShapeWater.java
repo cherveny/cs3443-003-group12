e7035e754576d17797ad60ede9c50dfdbf49dd48
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 05:00:04 -0500

    Update ShapeWater.java

--- Cantrips/ShapeWater.java
+++ Cantrips/ShapeWater.java
@@ -2,6 +2,8 @@ public class ShapeWater extends Spell {
   
   private int Level = 0;
   
+  private String name = "Shape Water";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Shape Water\n"+
@@ -12,7 +14,7 @@ public class ShapeWater extends Spell {
                        "School: Transmutation\n"+
                        "Attack/Save: None\n"+
                        "Damage/Effect: Control\n"+
-                       "Description: You choose an area of water that you can see within range and that fits within a 5-foot cube. You manipulate it in one of the following ways:\nYou instantaneously move or otherwise change the flow of the water as you direct, up to 5 feet in any direction. This movement doesn’t have enough force to cause damage.\nYou cause the water to form into simple shapes and animate at your direction. This change lasts for 1 hour.\nYou change the water’s color or opacity. The water must be changed in the same way throughout. This change lasts for 1 hour.\nYou freeze the water, provided that there are no creatures in it. The water unfreezes in 1 hour.\n\nIf you cast this spell multiple times, you can have no more than two of its non-instantaneous effects active at a time, and you can dismiss such an effect as an action.";
+                       "Description: You choose an area of water that you can see within range and that fits within a 5-foot cube. You manipulate it in one of the following ways:\nYou instantaneously move or otherwise change the flow of the water as you direct, up to 5 feet in any direction. This movement doesnâ€™t have enough force to cause damage.\nYou cause the water to form into simple shapes and animate at your direction. This change lasts for 1 hour.\nYou change the waterâ€™s color or opacity. The water must be changed in the same way throughout. This change lasts for 1 hour.\nYou freeze the water, provided that there are no creatures in it. The water unfreezes in 1 hour.\n\nIf you cast this spell multiple times, you can have no more than two of its non-instantaneous effects active at a time, and you can dismiss such an effect as an action.";
 
     
   public ShapeWater() {
@@ -28,6 +30,11 @@ public class ShapeWater extends Spell {
 public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
