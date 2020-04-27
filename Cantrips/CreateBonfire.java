4152d24b154ea45292fb4cae5efbc14ea64ee5b6
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 04:47:09 -0500

    same as others

--- Cantrips/CreateBonfire.java
+++ Cantrips/CreateBonfire.java
@@ -2,6 +2,8 @@ public class CreateBonfire extends Spell {
   
   private int Level = 0;
   
+  private String name = "Create Bonfire";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Create Bonfire\n"+
@@ -12,7 +14,7 @@ public class CreateBonfire extends Spell {
                        "School: Conjuration\n"+
                        "Attack/Save: DEX Save\n"+
                        "Damage/Effect: Fire\n"+
-                       "Description: You create a bonfire on ground that you can see within range. Until the spell ends, the magic bonfire fills a 5-foot cube. Any creature in the bonfire’s space when you cast the spell must succeed on a Dexterity saving throw or take 1d8 fire damage. A creature must also make the saving throw when it moves into the bonfire’s space for the first time on a turn or ends its turn there.\nThe bonfire ignites flammable objects in its area that aren’t being worn or carried.\nThe spell’s damage increases by 1d8 when you reach 5th level (2d8), 11th level (3d8), and 17th level (4d8).";
+                       "Description: You create a bonfire on ground that you can see within range. Until the spell ends, the magic bonfire fills a 5-foot cube. Any creature in the bonfireâ€™s space when you cast the spell must succeed on a Dexterity saving throw or take 1d8 fire damage. A creature must also make the saving throw when it moves into the bonfireâ€™s space for the first time on a turn or ends its turn there.\nThe bonfire ignites flammable objects in its area that arenâ€™t being worn or carried.\nThe spellâ€™s damage increases by 1d8 when you reach 5th level (2d8), 11th level (3d8), and 17th level (4d8).";
   public CreateBonfire() {
     
   }
@@ -26,6 +28,11 @@ public class CreateBonfire extends Spell {
 public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
