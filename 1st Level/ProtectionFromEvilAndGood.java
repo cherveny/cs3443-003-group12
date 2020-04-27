adb7cd999127bff7943f058171e1c0f8952eac23
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:40:33 -0500

    Update ProtectionFromEvilAndGood.java

--- 1st Level/ProtectionFromEvilAndGood.java
+++ 1st Level/ProtectionFromEvilAndGood.java
@@ -2,6 +2,8 @@ public class ProtectionFromEvilAndGood extends Spell {
   
   private int Level = 1;
   
+  private String name = "Protection from Evil and Good";
+  
   private String ClassRequirement[] = {"Cleric","Wizard"};
   
   private String Description = "Spell: Protection from Evil and Good\n"+
@@ -29,6 +31,11 @@ public class ProtectionFromEvilAndGood extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
