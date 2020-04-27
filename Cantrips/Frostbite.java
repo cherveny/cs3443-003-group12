68171957f43f7c6ddf7a0578bb284cc6f6ad27ab
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 04:49:58 -0500

    Update Frostbite.java

--- Cantrips/Frostbite.java
+++ Cantrips/Frostbite.java
@@ -2,6 +2,8 @@ public class Frostbite extends Spell {
   
   private int Level = 0;
   
+  private String name = "Frostbite";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Frostbite\n"+
@@ -12,7 +14,7 @@ public class Frostbite extends Spell {
                        "School: Evocation\n"+
                        "Attack/Save: CON Save\n"+
                        "Damage/Effect: Cold\n"+
-                       "Description: You cause numbing frost to form on one creature that you can see within range. The target must make a Constitution saving throw. On a failed save, the target takes 1d6 cold damage, and it has disadvantage on the next weapon attack roll it makes before the end of its next turn.\nThe spell’s damage increases by 1d6 when you reach 5th level (2d6), 11th level (3d6), and 17th level (4d6).";
+                       "Description: You cause numbing frost to form on one creature that you can see within range. The target must make a Constitution saving throw. On a failed save, the target takes 1d6 cold damage, and it has disadvantage on the next weapon attack roll it makes before the end of its next turn.\nThe spellâ€™s damage increases by 1d6 when you reach 5th level (2d6), 11th level (3d6), and 17th level (4d6).";
   public Frostbite() {
     
   }
@@ -21,6 +23,11 @@ public class Frostbite extends Spell {
   public int getLevel()
   {
     return this.Level;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
 public String[] getClassRequirement()
