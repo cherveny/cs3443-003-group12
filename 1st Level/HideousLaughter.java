37bce7c7958d1874979b3c9ffc821898a563ce4c
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:30:00 -0500

    Update HideousLaughter.java

--- 1st Level/HideousLaughter.java
+++ 1st Level/HideousLaughter.java
@@ -2,6 +2,8 @@ public class HideousLaughter extends Spell {
   
   private int Level = 1;
   
+  private String name = "Hideous Laughter";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Hideous Laughter\n"+
@@ -29,6 +31,11 @@ public class HideousLaughter extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
