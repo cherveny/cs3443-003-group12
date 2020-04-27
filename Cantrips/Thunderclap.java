ee4be0650ea2707e75138b705fe6cc0254fe8c65
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 05:03:29 -0500

    Update Thunderclap.java

--- Cantrips/Thunderclap.java
+++ Cantrips/Thunderclap.java
@@ -2,6 +2,8 @@ public class Thunderclap extends Spell {
   
   private int Level = 0;
   
+  private String name = "Thunderclap";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Thunderclap\n"+
@@ -12,7 +14,7 @@ public class Thunderclap extends Spell {
                        "School: Evocation\n"+
                        "Attack/Save: CON Save\n"+
                        "Damage/Effect: Thunder\n"+
-                       "Description: You create a burst of thunderous sound that can be heard up to 100 feet away. Each creature within range, other than you, must succeed on a Constitution saving throw or take 1d6 thunder damage.\nThe spell’s damage increases by 1d6 when you reach 5th level (2d6), 11th level (3d6), and 17th level (4d6).";
+                       "Description: You create a burst of thunderous sound that can be heard up to 100 feet away. Each creature within range, other than you, must succeed on a Constitution saving throw or take 1d6 thunder damage.\nThe spellâ€™s damage increases by 1d6 when you reach 5th level (2d6), 11th level (3d6), and 17th level (4d6).";
   public Thunderclap() {
     
   }
@@ -26,6 +28,11 @@ public class Thunderclap extends Spell {
 public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
