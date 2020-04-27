a81365abbb08193a97a354a326d2f029557df294
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:21:29 -0500

    Update EarthTremor.java

--- 1st Level/EarthTremor.java
+++ 1st Level/EarthTremor.java
@@ -2,6 +2,8 @@ public class EarthTremor extends Spell {
   
   private int Level = 1;
   
+  private String name = "Earth Tremor";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Earth Tremor\n"+
@@ -29,6 +31,11 @@ public class EarthTremor extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
