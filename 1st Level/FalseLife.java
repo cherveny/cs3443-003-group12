1e3ae45ad7c4dbc309344c2b5ececf525b689bcd
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:23:31 -0500

    Update FalseLife.java

--- 1st Level/FalseLife.java
+++ 1st Level/FalseLife.java
@@ -2,6 +2,8 @@ public class FalseLife extends Spell {
   
   private int Level = 1;
   
+  private String name = "False Life";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: False Life\n"+
@@ -29,6 +31,11 @@ public class FalseLife extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
