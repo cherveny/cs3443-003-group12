ce728daeb25b4d84b4e550a7263b452d0df001f9
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:44:03 -0500

    Update Shield.java

--- 1st Level/Shield.java
+++ 1st Level/Shield.java
@@ -2,6 +2,8 @@ public class Shield extends Spell {
   
   private int Level = 1;
   
+  private String name = "Shield";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Shield\n"+
@@ -29,6 +31,11 @@ public class Shield extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
