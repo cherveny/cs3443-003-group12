b18e2dd7870ed8f4f83a6c5371febaf207d0e1d5
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:35:23 -0500

    Update Jump.java

--- 1st Level/Jump.java
+++ 1st Level/Jump.java
@@ -2,6 +2,8 @@ public class Jump extends Spell {
   
   private int Level = 1;
   
+  private String name = "Jump";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Jump\n"+
@@ -29,6 +31,11 @@ public class Jump extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
