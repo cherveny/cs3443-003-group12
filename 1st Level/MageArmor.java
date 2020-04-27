b9b421d573d68dc389aa29cb262fa8a08dbfe698
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:37:04 -0500

    Update MageArmor.java

--- 1st Level/MageArmor.java
+++ 1st Level/MageArmor.java
@@ -2,6 +2,8 @@ public class MageArmor extends Spell {
   
   private int Level = 1;
   
+  private String name = "Mage Armor";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Mage Armor\n"+
@@ -29,6 +31,11 @@ public class MageArmor extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
