1ced970ecc7bb923c856c1b2c708d4073693ec93
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 04:51:24 -0500

    same as others

--- Cantrips/Gust.java
+++ Cantrips/Gust.java
@@ -2,6 +2,8 @@ public class Gust extends Spell {
   
   private int Level = 0;
   
+  private String name = "Gust";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Gust\n"+
@@ -12,7 +14,7 @@ public class Gust extends Spell {
                        "School: Transmutation\n"+
                        "Attack/Save: STR Save\n"+
                        "Damage/Effect: Control\n"+
-                       "Description: You seize the air and compel it to create one of the following effects at a point you can see within range:\nOne Medium or smaller creature that you choose must succeed on a Strength saving throw or be pushed up to 5 feet away from you.\nYou create a small blast of air capable of moving one object that is neither held nor carried and that weighs no more than 5 pounds. The object is pushed up to 10 feet away from you. It isn’t pushed with enough force to cause damage.\nYou create a harmless sensory effect using air, such as causing leaves to rustle, wind to slam shutters shut, or your clothing to ripple in a breeze.";
+                       "Description: You seize the air and compel it to create one of the following effects at a point you can see within range:\nOne Medium or smaller creature that you choose must succeed on a Strength saving throw or be pushed up to 5 feet away from you.\nYou create a small blast of air capable of moving one object that is neither held nor carried and that weighs no more than 5 pounds. The object is pushed up to 10 feet away from you. It isnâ€™t pushed with enough force to cause damage.\nYou create a harmless sensory effect using air, such as causing leaves to rustle, wind to slam shutters shut, or your clothing to ripple in a breeze.";
   public Gust() {
     
   }
@@ -26,6 +28,11 @@ public class Gust extends Spell {
 public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
