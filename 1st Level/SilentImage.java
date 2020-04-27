5d873fd105739c56d353b556459c74da881960e1
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:45:54 -0500

    Update SilentImage.java

--- 1st Level/SilentImage.java
+++ 1st Level/SilentImage.java
@@ -2,6 +2,8 @@ public class SilentImage extends Spell {
   
   private int Level = 1;
   
+  private String name = "Silent Image";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Silent Image\n"+
@@ -29,6 +31,11 @@ public class SilentImage extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
