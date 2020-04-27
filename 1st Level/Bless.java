457ffa961653c119f3aecf903540c3398938245f
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:06:38 -0500

    Update Bless.java

--- 1st Level/Bless.java
+++ 1st Level/Bless.java
@@ -2,6 +2,8 @@ public class Bless extends Spell {
   
   private int Level = 1;
   
+  private String name = "Bless" ;
+  
   private String ClassRequirement[] = {"Cleric"};
   
   private String Description = "Spell: Bless\n"+
@@ -29,6 +31,11 @@ public class Bless extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+  
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
