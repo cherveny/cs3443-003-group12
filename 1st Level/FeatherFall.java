7d1252d242ebbee6228eda397caebae8c8a84367
MezaCode <60710916+MezaCode@users.noreply.github.com>
2020-04-27 06:24:15 -0500

    Update FeatherFall.java

--- 1st Level/FeatherFall.java
+++ 1st Level/FeatherFall.java
@@ -2,6 +2,8 @@ public class FeatherFall extends Spell {
   
   private int Level = 1;
   
+  private String name = "Feather Fall";
+  
   private String ClassRequirement[] = {"Wizard"};
   
   private String Description = "Spell: Feather Fall\n"+
@@ -29,6 +31,11 @@ public class FeatherFall extends Spell {
   public String[] getClassRequirement()
   {
     return this.ClassRequirement;
+  }
+    
+  public String getName()
+  {
+    return name;
   }
   
   public String getDescription()
